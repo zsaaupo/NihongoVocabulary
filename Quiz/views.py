48054fc5943b5b9fed18c7349e329db84003952f
zsaaupo <zakariasarker407@gmail.com>
2022-07-08 05:20:09 +0600

    added random quarry for equiz and nquiz

--- Quiz/views.py
+++ Quiz/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import QData
+import random
 
 
 def choice(requset):
@@ -7,13 +8,19 @@ def choice(requset):
     return render(requset, 'choice.html')
 
 
+# added random quarry for equiz and nquiz
+
 def equiz(requset):
-    qdate = QData.objects.filter().all()
-    return render(requset, 'english.html', {'i': qdate})
+    qdata = list(QData.objects.all())
+    qdatar = random.sample(qdata, len(qdata))
+    return render(requset, 'english.html', {'i': qdatar})
+
 
 def nquiz(requset):
-    qdate = QData.objects.filter().all()
-    return render(requset, 'nihongo.html', {'i': qdate})
+    qdata = list(QData.objects.all())
+    qdatar = random.sample(qdata, len(qdata))
+    return render(requset, 'nihongo.html', {'i': qdatar})
+
 
 def quiz(requset, get_id):
     qdate = QData.objects.filter(id=get_id).first()
